2e5142cd79c2f9c20f176648ee631ccb129368f9
JC Sindo <digitaljoni@gmail.com>
2022-08-06 09:02:07 +0800

    add id on RecipeDetailsPage

--- lib/recipe_details/widgets/recipe_image.dart
+++ lib/recipe_details/widgets/recipe_image.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:seefood/common/common.dart';
+import 'package:seefood/config/app_theme.dart';
 
 class RecipeImage extends StatelessWidget {
   const RecipeImage({
@@ -14,17 +15,38 @@ class RecipeImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Hero(
-      tag: 'recipe$id',
-      child: CachedNetworkImage(
-        imageUrl: imageUrl,
-        progressIndicatorBuilder: (context, url, downloadProgress) =>
-            const Loading(),
-        errorWidget: (context, url, error) => const Icon(Icons.error),
-        height: MediaQuery.of(context).size.height / 3,
-        width: double.infinity,
-        fit: BoxFit.cover,
-      ),
+    final imageHeight = MediaQuery.of(context).size.height / 3;
+    return Stack(
+      children: [
+        Hero(
+          tag: 'recipe$id',
+          child: CachedNetworkImage(
+            imageUrl: imageUrl,
+            progressIndicatorBuilder: (context, url, downloadProgress) =>
+                const Loading(),
+            errorWidget: (context, url, error) => const Icon(Icons.error),
+            height: imageHeight,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Container(
+          color: Colors.white12,
+          height: imageHeight,
+          child: Align(
+            alignment: Alignment.bottomRight,
+            child: Text(
+              '#$id',
+              style: const TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 70,
+                height: 0.01,
+                color: AppTheme.mobileBackgroundColor,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
